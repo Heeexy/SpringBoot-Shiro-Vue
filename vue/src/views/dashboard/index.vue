4eab881c39b5848d58d536cf20dc3d614b4d25f7
Heeexy <hexiaoyu_job@163.com>
2018-03-07 22:35:56 +0800

    update dashboard

--- vue/src/views/dashboard/index.vue
+++ vue/src/views/dashboard/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="dashboard-container">
-    首页
+    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin: 40px"
+      src="http://ots7yt7am.bkt.clouddn.com/file/github.png"></a>
   </div>
 </template>
 <script>
   export default {
     name: 'dashboard',
-
     data() {
       return {}
     },
-
   }
 </script>
