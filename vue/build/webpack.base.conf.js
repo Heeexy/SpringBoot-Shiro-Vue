7b8437d5bb8ff2955b8075a342a856e2c093b437
heeexy <heeexy@gmail.com>
2020-05-08 14:17:31 +0800

    1. update version of dependency 2. add HomePageController

--- vue/build/webpack.base.conf.js
+++ vue/build/webpack.base.conf.js
@@ -80,7 +80,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 100000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
